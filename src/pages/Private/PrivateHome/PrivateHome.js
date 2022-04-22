d89f00b7b0bd0ee946bb722c896a1af29a1abbc9
faridgi <89008266+faridgi@users.noreply.github.com>
2022-04-22 16:33:42 +0200

    authentification react firebase

---
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import cat from "./cat.gif"
+import nft from "./nft.gif"
+
+export default function PrivateHome() {
+  return (
+    <div className='container p-5'>
+        <h1 className="display-3 text-light mb-4">Home Sweet Private Home</h1>
+        {/* <img src={cat} /> */}
+        <img src={nft} />
+
+    </div>
+  )
+}
